10ee0c5bb55a29a88acb31d3bd63505200d6a0d4
zhaokat <katiezhao25@gmail.com>
2021-09-02 22:49:37 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,27 @@
+$(function () {
+  console.log('Hello Bootstrap5');
+});
+//
+
+//
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "3",
+  centeredSlides: false,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    1000: {
+      slidesPerView: 3
+    },
+    768: {
+      slidesPerView: 2
+    },
+    300: {
+      slidesPerView: 1
+    }
+  }
+});
